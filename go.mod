a3af2cdb7b0066ec1b9b652ffa3d9fe0a562ddc9
tc80 <tylercaslin47@gmail.com>
2020-12-02 21:52:27 +0000

    credentials set up with simple translation example

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module Live-Stream-Translator
 
 go 1.15
+
+require (
+	cloud.google.com/go v0.72.0
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
+	golang.org/x/oauth2 v0.0.0-20201109201403-9fd604954f58 // indirect
+	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3 // indirect
+	google.golang.org/api v0.35.0
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201202151023-55d61f90c1ce
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	translate "cloud.google.com/go/translate/apiv3"
+	"google.golang.org/api/option"
+	translatepb "google.golang.org/genproto/googleapis/cloud/translate/v3"
+)
+
+const (
+	url              = "https://www.youtube.com/watch?v=5qap5aO4i9A"
+	translationScope = "https://www.googleapis.com/auth/cloud-translation"
+	projectID        = "live-stream-translator"
+	location         = "global"
+)
+
+var (
+	parent = fmt.Sprintf("projects/%s/locations/%s", projectID, location)
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func translateText(client *translate.TranslationClient, contents []string, src, dst string) (*translatepb.TranslateTextResponse, error) {
+	return client.TranslateText(context.Background(), &translatepb.TranslateTextRequest{
+		Parent:             parent,
+		Contents:           contents,
+		SourceLanguageCode: src,
+		TargetLanguageCode: dst,
+	})
+}
 
 func main() {
-	fmt.Println("hello world")
+	ctx := context.Background()
+	client, err := translate.NewTranslationClient(ctx, option.WithCredentialsFile("credentials.json"))
+	check(err)
+	defer client.Close()
+
+	resp, err := translateText(client, []string{"hello", "world"}, "en-US", "bg")
+	check(err)
+
+	fmt.Println(resp.String())
 }
